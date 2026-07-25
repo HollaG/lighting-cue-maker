5d1466194648d4724c76353d17dee8a2925d6535
Marcus <marcussoh38@gmail.com>
2026-07-25 16:22:23 +0800

    chore(fe): refactor CSS

--- website/src/components/RichContent/RichWord.tsx
+++ website/src/components/RichContent/RichWord.tsx
@@ -3,6 +3,7 @@ import { Group, Box, Text, Center } from "@mantine/core";
 import clsx from "clsx";
 import classes from "./RichWord.module.css";
 import { convertUuidForDatabase } from "../../utils/convertUuid";
+import { useAppStore } from "../../store/appStore";
 
 interface RichWordProps {
   word: string;
@@ -13,6 +14,8 @@ interface RichWordProps {
 }
 
 const RichWordInternal = ({ word, index1, index2, cueNumber, isSelected }: RichWordProps) => {
+  const inputMode = useAppStore((s) => s.inputMode);
+  console.log(inputMode);
   if (word === "-") return <Text variant="lyric">{word}</Text>;
   if (word.startsWith("<cueId=")) {
     // stand-alone
@@ -23,15 +26,22 @@ const RichWordInternal = ({ word, index1, index2, cueNumber, isSelected }: RichW
           id={`ref-${cueId}`}
           data-action="select-cue"
           data-cue-id={cueId}
-          style={{
-            paddingLeft: "0.5rem",
-            paddingRight: "0.5rem",
-            height: "stretch",
-            backgroundColor: "yellow",
-            border: "4px solid yellow",
-            cursor: "pointer",
-          }}
-          className={clsx(classes["cue"], classes["cue-wrapper"], isSelected ? classes["cue-selected"] : "")}
+          className={clsx(
+            classes["richContainer"],
+            classes["is-cue"],
+            classes["indicator"],
+            isSelected ? classes["selected"] : "",
+          )}
+
+          // style={{
+          //   paddingLeft: "0.5rem",
+          //   paddingRight: "0.5rem",
+          //   height: "stretch",
+          //   backgroundColor: "yellow",
+          //   border: "4px solid yellow",
+          //   cursor: "pointer",
+          // }}
+          // className={clsx(classes["cue"], classes["cue-wrapper"], isSelected ? classes["cue-selected"] : "")}
         >
           Cue {cueNumber}
         </Center>
@@ -47,13 +57,12 @@ const RichWordInternal = ({ word, index1, index2, cueNumber, isSelected }: RichW
         data-action="select-cue"
         data-cue-id={cueId}
         gap={0}
-        style={{ cursor: "pointer" }}
-        className={clsx(classes["cue-wrapper"], isSelected ? classes["cue-selected"] : "")}
+        className={clsx(classes["richContainer"], classes["is-cue"], isSelected ? classes["selected"] : "")}
       >
-        <Text variant="lyric" className={clsx(classes["lyric"], classes["in-cue"])}>
+        <Text variant="lyric" className={clsx(classes["lyric"])}>
           {textContent}
         </Text>
-        <Box className={classes["cue"]}>
+        <Box className={classes["indicator"]}>
           <Text>Cue {cueNumber}</Text>
         </Box>
       </Group>
@@ -66,8 +75,7 @@ const RichWordInternal = ({ word, index1, index2, cueNumber, isSelected }: RichW
           data-line-index={index1}
           data-word-index={index2}
           data-is-space="true"
-          className={classes["space"]}
-          style={{ width: "calc(1rem / 2)", height: "stretch" }}
+          className={clsx(classes["space"], classes[`mode-${inputMode}`])}
         >
           ㅤ
         </Box>
@@ -81,7 +89,7 @@ const RichWordInternal = ({ word, index1, index2, cueNumber, isSelected }: RichW
         data-word-index={index2}
         data-is-space="false"
         variant="lyric"
-        className={classes["lyric"]}
+        className={clsx(classes["lyric"], classes[`mode-${inputMode}`])}
       >
         {word}
       </Text>
